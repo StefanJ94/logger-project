46a6f3d51d416fe5dc7e3525972bddb19be0cef2
stefanJ94 <jovanovic.stefan94@yahoo.com>
2023-11-20 11:55:37 +0100

    First commit

---
+++ src/main/java/com/example/LoggerProject/LoggerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoggerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoggerProject/LoggerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoggerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
